2442c747836ca7073dfc70f43b31054d7796d973
suhassamuel <suhas.sahaj1@gmail.com>
2021-07-13 14:10:44 -0600

    locator added

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,8 +1,14 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
+	private By carBtn = By.id("cart");
+	
 	public void getCart() {
-		System.out.println("get cart");
+		
+	
+		System.out.println("get cart"+carBtn);
 	}
 }
